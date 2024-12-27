fee6066e32cc7898398af44da15f09529a5ad4d9
mdmnky13 <mdmnky13@gmail.com>
2024-12-27 03:14:52 -0800

    Init

---
+++ pfUI-quickfocus.lua
@@ -0,0 +1,23 @@
+BINDING_HEADER_QUICKFOCUS = "QuickFocus"
+BINDING_NAME_QUICKFOCUS_SET = "Set Focus"
+BINDING_NAME_QUICKFOCUS_TARGET = "Target Focus"
+
+
+function QuickFocus_Set()
+  if not pfUI.uf or not pfUI.uf.focus then return end
+
+  if UnitName("target") then
+    pfUI.uf.focus.unitname = strlower(UnitName("target"))
+  else
+    pfUI.uf.focus.unitname = nil
+    pfUI.uf.focus.label = nil
+  end
+end
+
+function QuickFocus_Target()
+  if pfUI.uf and pfUI.uf.focus then
+    if pfUI.uf.focus.unitname then
+      TargetByName(pfUI.uf.focus.unitname, true)
+    end
+  end
+end
